f4a095115ed0d72e264d8d78da943632349a6f5a
jan <jannos1999@gmail.com>
2024-12-29 16:47:41 +0100

    add manifest.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'dance-player'
+  base: 'dance-player',
+  build: {
+    manifest: true,
+  },
 })
